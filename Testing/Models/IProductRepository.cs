11547157fd139274fe44c1c6be0abfe9c85f63f4
Rakin Brown <rakinbrown38@gmail.com>
2023-07-24 15:32:32 -0500

    ASPNET setup and connection

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing.Models
+{
+	public interface IProductRepository
+	{
+        public IEnumerable<Product> GetAllProducts();
+    }
+
+	
+}
+
