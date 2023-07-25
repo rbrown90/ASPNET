df66b76768c3d75d37bac9b3c63eb5d2f51a039b
Rakin Brown <rakinbrown38@gmail.com>
2023-07-24 19:54:07 -0500

    Create Product

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -5,14 +5,14 @@ using Dapper;
 
 namespace Testing.Models
 {
-	public class ProductRepository : IProductRepository
-	{
+    public class ProductRepository : IProductRepository
+    {
         private readonly IDbConnection _conn;
 
-		public ProductRepository(IDbConnection conn)
-		{
+        public ProductRepository(IDbConnection conn)
+        {
             _conn = conn;
-		}
+        }
 
         public IEnumerable<Product> GetAllProducts()
         {
@@ -22,13 +22,32 @@ namespace Testing.Models
         public Product GetProduct(int id)
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE ProductID = @id",
-                new {id = id});
+                new { id = id });
         }
 
         public void UpdateProduct(Product product)
         {
             _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
-            new {name = product.Name, price = product.Price, productID = product.ProductID});
+            new {name = product.Name, price = product.Price, productID = product.ProductID });
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categortyID);",
+                new {name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
         }
     }
 }
